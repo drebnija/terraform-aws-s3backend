ab7da0d11ded3652410556adaed42e460c3c6de2
EC2 Default User <ec2-user@ip-172-31-9-165.eu-west-1.compute.internal>
2022-04-13 12:04:07 +0000

    4tht

--- iam.tf
+++ iam.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "policy_doc" {
   }
 
   statement {
-    actions = ["s3:GetOblect", "s3:PutObject", "s3:DeleteObject"]
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
 
     resources = ["${aws_s3_bucket.s3_bucket.arn}/*", ]
   }
